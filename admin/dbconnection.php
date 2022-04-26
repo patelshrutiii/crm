adb15d3e0275d2382c112cc022d645a1f7725519
patelshrutiii <98640689+patelshrutiii@users.noreply.github.com>
2022-04-26 16:50:16 +0530

    Add files via upload

---
+++ admin/dbconnection.php
@@ -0,0 +1,8 @@
+<?php
+// $con=mysqli_connect("localhost", "root", "shruti", "crm");
+$con=mysqli_connect("sql312.epizy.com", "epiz_31600976", "Mq7yndK1yV", "epiz_31600976_XXX");
+if(mysqli_connect_errno()){
+echo "Connection Fail".mysqli_connect_error();
+}
+
+?>
